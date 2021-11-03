92c05467552213b707dc81df6eb8c22f0afdea1a
Ilona Shakurova <leverseau19@gmail.com>
2021-11-03 10:52:24 +0200

    Modify memcpy, add insigned char

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,13 +14,13 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t		i;
-	char		*d;
-	const char	*s;
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
 
 	i = 0;
-	d = dst;
-	s = src;
+	d = (char *)dst;
+	s = (char *)src;
 	while (i < n)
 	{
 		*d++ = *s++;
