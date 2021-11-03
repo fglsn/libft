5826025dfa0d442a7d27693647a83b53f4b4c743
Ilona Shakurova <leverseau19@gmail.com>
2021-11-03 20:22:32 +0200

    Add strstr with test, check norm

--- ft_strstr.c
+++ ft_strstr.c
@@ -1,35 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/03 18:30:11 by ishakuro          #+#    #+#             */
-/*   Updated: 2021/11/03 20:03:54 by ishakuro         ###   ########.fr       */
+/*   Created: 2021/11/03 19:36:10 by ishakuro          #+#    #+#             */
+/*   Updated: 2021/11/03 20:08:05 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strstr(const char *haystack, const char *needle)
 {
-	char		ch;
-	const char	*ptr;
+	int	i;
+	int	j;
+	int	stack_len;
+	int	needle_len;
 
-	ch = (char)c;
-	ptr = NULL;
-	while (*s)
+	i = 0;
+	stack_len = ft_strlen(haystack);
+	needle_len = ft_strlen(needle);
+	if (needle_len == 0)
+		return ((char *)haystack);
+	while (i <= stack_len - needle_len)
 	{
-		if (*s == ch)
+		j = 0;
+		while (haystack[j + i] == needle[j] && j < needle_len)
 		{
-			ptr = s;
+			j++;
 		}
-		s++;
+		if (j == needle_len)
+		{
+			return ((char *)&haystack[i]);
+		}
+		i++;
 	}
-	if (*s == '\0' && ch == '\0')
-	{
-		return ((char *)s);
-	}
-	return ((char *)ptr);
+	return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:05:58 by ishakuro          #+#    #+#             */
-/*   Updated: 2021/11/03 18:29:48 by ishakuro         ###   ########.fr       */
+/*   Updated: 2021/11/03 19:37:57 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ char	*ft_strncat(char *s1, const char *s2, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strstr(const char *haystack, const char *needle);
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
