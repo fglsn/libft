689c8cd6dc279e4874322c61a96ab9e9d3519b6b
Ilona Shakurova <ishakuro@c3r5p1.hive.fi>
2021-11-08 12:39:14 +0200

    Check norm ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 17:22:05 by ishakuro          #+#    #+#             */
-/*   Updated: 2021/11/05 17:38:43 by ishakuro         ###   ########.fr       */
+/*   Updated: 2021/11/08 12:15:05 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,12 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
-	unsigned char		ch;
-	size_t				i;
-	
+	unsigned char	ch;
+	size_t			i;
+
 	i = 0;
-	str = (unsigned char*)s;
+	str = (unsigned char *)s;
 	ch = (unsigned char)c;
-	
 	while (n > i)
 	{
 		if (*str == ch)
