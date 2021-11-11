8c178207ff3d3602c1922fa3c575d97c519d4c41
Ilona Shakurova <leverseau19@gmail.com>
2021-11-11 09:32:06 +0200

    Modify strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,37 +6,44 @@
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:03:32 by ishakuro          #+#    #+#             */
-/*   Updated: 2021/11/10 17:15:42 by ishakuro         ###   ########.fr       */
+/*   Updated: 2021/11/11 09:30:28 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+static size_t	s_len(char const *s)
+{
+	size_t	len;
+
+	if (s)
+		len = ft_strlen(s);
+	else
+		len = 0;
+	return (len);
+}
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
 	char	*start;
+	size_t	len_1;
+	size_t	len_2;
 
 	if (!s1 && !s2)
 		return (NULL);
-	result = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+	len_1 = s_len(s1);
+	len_2 = s_len(s2);
+	result = ft_strnew(len_1 + len_2);
 	if (!result)
 		return (NULL);
 	start = result;
 	if (s1)
-	{
 		while (*s1)
-		{
 			*result++ = *s1++;
-		}
-	}
 	if (s2)
-	{
 		while (*s2)
-		{
 			*result++ = *s2++;
-		}
-	}
 	return (start);
 }
