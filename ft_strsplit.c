ff5dc4fc42c0c09b3d50dbb567b4ff10725a234a
Ilona Shakurova <leverseau19@gmail.com>
2021-11-14 18:19:37 +0200

    Refactor ft_strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsplit2.c                                     :+:      :+:    :+:   */
+/*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 15:58:36 by ishakuro          #+#    #+#             */
-/*   Updated: 2021/11/12 13:34:49 by ishakuro         ###   ########.fr       */
+/*   Updated: 2021/11/14 18:16:49 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,78 +14,58 @@
 
 static size_t	ft_count_words(char const *s, char del)
 {
-	size_t	i;
 	size_t	word;
 	size_t	word_count;
 
-	i = 0;
 	word = 0;
 	word_count = 0;
-	while (s[i])
+	while (*s)
 	{
-		if (s[i] != del)
+		if (*s != del)
 		{
 			word++;
 			if (word == 1)
-			{
 				word_count++;
-			}
 		}
 		else
 			word = 0;
-		i++;
+		s++;
 	}
 	return (word_count);
 }
 
-static size_t	ft_word_length(char const *s, size_t i, char del)
+static size_t	ft_word_length(char const *s, char del)
 {
 	size_t	len;
 
 	len = 0;
-	while (s[i] && s[i] != del)
+	while (*s && *s != del)
 	{
 		len++;
-		i++;
+		s++;
 	}
 	return (len);
 }
 
-int	ft_strcpy_split(char *dst, const char *src, size_t i, char del)
-{
-	size_t	n;
-
-	n = 0;
-	while (src[i] && src[i] != del)
-	{
-		dst[n++] = src[i++];
-	}
-	dst[n] = '\0';
-	return (i);
-}
-
 char	**ft_split(char *s, char c, char **result)
 {
-	size_t	i;
 	size_t	j;
+	size_t	word_len;
 
-	i = 0;
 	j = 0;
-	while (s[i])
+	while (*s)
 	{
-		while (s[i] && s[i] == c)
-			i++;
-		if (!(ft_word_length(s, i, c)))
+		while (*s && *s == c)
+			s++;
+		word_len = ft_word_length(s, c);
+		if (!word_len)
 			return (result);
-		result[j] = malloc(sizeof(char *) * (ft_word_length(s, i, c) + 1));
+		result[j] = malloc(sizeof(char *) * (word_len + 1));
 		if (!result[j])
 			return (NULL);
-		if (s[i])
-		{
-			i = ft_strcpy_split(result[j], s, i, c);
-			j++;
-		}
-		result[j] = 0;
+		ft_strncpy(result[j], s, word_len);
+		s = s + word_len;
+		j++;
 	}
 	return (result);
 }
