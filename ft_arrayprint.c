279596217a8938181cdfd3716d1066a8e443dd2c
Ilona Shakurova <leverseau19@gmail.com>
2022-01-13 18:34:19 +0200

    Add comments to arrayprint

--- ft_arrayprint.c
+++ ft_arrayprint.c
@@ -6,11 +6,15 @@
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 18:06:24 by ishakuro          #+#    #+#             */
-/*   Updated: 2022/01/13 18:08:08 by ishakuro         ###   ########.fr       */
+/*   Updated: 2022/01/13 18:24:27 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+ * This only valid if last element of the array was set to 0. 
+ */
 
 void	ft_printarray(char **content)
 {
